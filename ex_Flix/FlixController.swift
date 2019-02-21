9b7acfab28013cf266c8efad3353465196d553f6
Fung Yung <fungyung1225@gmail.com>
2019-02-20 22:47:13 -0500

    added new view controller name FlixController

---
+++ ex_Flix/FlixController.swift
@@ -0,0 +1,43 @@
+//
+//  FlixController.swift
+//  ex_Flix
+//
+//  Created by Fung on 2/20/19.
+//  Copyright © 2019 fungyung. All rights reserved.
+//
+
+import UIKit
+
+class FlixController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
