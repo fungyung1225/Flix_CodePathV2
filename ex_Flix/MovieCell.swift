ff35fd32aabd77f89af58065a4e8119a0c73821d
Fung Yung <fungyung1225@gmail.com>
2019-02-20 23:03:21 -0500

    created TableViewCell class for holding movie info in cell

---
+++ ex_Flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  ex_Flix
+//
+//  Created by Fung on 2/20/19.
+//  Copyright © 2019 fungyung. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterImg: UIImageView!
+    @IBOutlet weak var nameLbl: UIView!
+    @IBOutlet weak var desLal: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
